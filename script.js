928ce3dd2dd4664dfa585ab9205db4d8e7ca7c68
Theo <theophileca@gmail.com>
2021-07-23 08:25:38 -0400

    landscape layout

--- script.js
+++ script.js
@@ -18,19 +18,3 @@ function lItem ()
         document.getElementById("days").value=displaynumber;
         }
 
-
-        function lock (orientation) {
-            // Go into full screen first
-            if (document.documentElement.requestFullscreen) {
-              document.documentElement.requestFullscreen();
-            } else if (document.documentElement.mozRequestFullScreen) {
-              document.documentElement.mozRequestFullScreen();
-            } else if (document.documentElement.webkitRequestFullscreen) {
-              document.documentElement.webkitRequestFullscreen();
-            } else if (document.documentElement.msRequestFullscreen) {
-              document.documentElement.msRequestFullscreen();
-            }
-          
-            // Then lock orientation
-            screen.orientation.lock(orientation);
-          }
