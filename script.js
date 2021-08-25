bba819c763b6c39890dc3671bd6402f2df0db886
Theo <theophileca@gmail.com>
2021-08-25 14:56:38 -0400

    Adding hours to the current date

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
 const millisecondsInDay = 24 * 60 * 60 * 1000;
 const numOfDayInMonth = 365 / 12;
+const numOfDayInHour = 1 / 24;
 
 function BeyondUseDate(date, days)
 {
     
     var numberOfMonthToAdd = document.getElementById("months").value;
     var numberOfDayToAdd = document.getElementById("days").value;
+    var numberOfHours = document.getElementById("hours").value;
     var totalNumberOfDay = numberOfMonthToAdd * numOfDayInMonth + numberOfDayToAdd;
+    var NumbertoAdd = numberOfHours * numOfDayInHour + totalNumberOfDay;
     var BUdate = Date.now();
-    BUdate += millisecondsInDay * totalNumberOfDay;
+         
+    BUdate += millisecondsInDay * NumbertoAdd;
     BUdate = new Date(BUdate);
-    document.getElementById('BUD').value = BUdate.toLocaleDateString();
+    document.getElementById('BUD').value = dayjs(BUdate).format('MM-DD-YY h:mm A');
 
 }
 
 
-  
